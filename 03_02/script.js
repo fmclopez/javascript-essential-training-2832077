39409b6883aadb5deb5beae914acb5b5b058c322
Frances Marjorie Lopez <frances_lopez@dlsu.edu.ph>
2022-12-13 22:41:19 +0800

    add notes and function creation trial

--- 03_02/script.js
+++ 03_02/script.js
@@ -2,21 +2,28 @@
  * Create a Backpack object.
  */
 
+// holds variable data & curly brackets {} define data as an object
 const backpack = {
+  // object properties -> defined by key-value pairs
   name: "Everyday Backpack",
   volume: 30,
   color: "grey",
   pocketNum: 15,
+  // sub-object with own properties
   strapLength: {
     left: 26,
     right: 26,
   },
   lidOpen: false,
+  // object methods -> properties containing function
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
+  },
+  ChangeName: function (newName) {
+    this.name = newName;
   },
 };
