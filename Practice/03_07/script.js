970a02a2dc38fea78e6e4a59d4ea373e771dfc3c
Frances Marjorie Lopez <frances_lopez@dlsu.edu.ph>
2022-12-14 16:21:37 +0800

    create Object practice done

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,27 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const game = {
+  name: "Pico Park",
+  size: 500,
+  players: "2-8 players",
+  characters: {
+    playerOne: "Blue",
+    playerTwo: "Red",
+    playerThree: "Yellow",
+    playerFour: "Pink",
+    playerFive: "Purple",
+    playerSix: "Brown",
+  },
+  rating: 10,
+  comment: "A Fun and chaotic game",
+};
+
+var x = "characters";
+
+console.log(game);
+console.log("This game is called ", game.name);
+console.log("The MB size of this game is", game["size"]);
+console.log("Player Five is color ->", game[x].playerFive);
+console.log(game["characters"]["playerTwo"]);
