3a0d4f091686951aaac6743822097c696f418ddf
Frances Marjorie Lopez <frances_lopez@dlsu.edu.ph>
2022-12-16 15:04:45 +0800

    did exercises for chapter 5

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,13 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+console.log(document.querySelectorAll("main figure"));
+
+console.log(document.querySelector("main figure"));
+
+// class css selector (.)
+console.log(document.querySelector(".site-title"));
+
+// id css selector (#)
+console.log(document.querySelector("#pack01"));
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,38 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+console.log(document.querySelector(".site-description"));
+
+console.log(document.querySelector(".site-description").className);
+
+console.log(document.querySelector(".site-description").classList);
+console.log(
+  document.querySelector(".site-description").classList.add("1st-class")
+);
+console.log(
+  document.querySelector(".site-description").classList.add("2nd-class")
+);
+console.log(
+  document.querySelector(".site-description").classList.remove("2nd-class")
+);
+
+console.log(
+  document
+    .querySelector(".site-description")
+    .setAttribute("src", "this-is-a-source-sample")
+);
+console.log(document.querySelector(".site-description").getAttribute("src"));
+console.log(
+  document
+    .querySelector(".site-description")
+    .setAttribute("src", "this-is-the-new-one")
+);
+
+console.log(
+  (document.querySelector(".site-description").style.color = "purple")
+);
+console.log(
+  (document.querySelector(".site-description").style.fontWeight = 600)
+);
+console.log(document.querySelector(".site-description").style);
